7de891810df3b631f03dd72760976c8dbc3a075f
esstephan <ericastephan@gmail.com>
2017-02-11 14:30:55 -0800

    takes in globalProjectArray as props

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,8 +5,9 @@ var Project = (props) => {
     <div className="project">
     <p>{props.projectName}</p>
     </div>
-    );
+    )
 }
-// In the ES6 spec, files are "modules" and do not share a top-level scope.
+
+
 // `var` declarations will only exist globally where explicitly defined.
 window.Project = Project;
